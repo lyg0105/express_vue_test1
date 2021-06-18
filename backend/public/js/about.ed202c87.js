1dcb5668ee4e63014ed46e5aec7d382fa55c99da
lyg0105 <lyg0105@naver.com>
2021-06-18 18:18:26 +0900

    first commit

---
+++ backend/public/js/about.ed202c87.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),c={class:"about"},o=Object(a["f"])("h1",null,"This is an about page",-1),u=Object(a["f"])("div",null," 초코콕 미니하이트쿠키 ",-1);function s(n,t){return Object(a["o"])(),Object(a["d"])("div",c,[o,u])}const b={};b.render=s;t["default"]=b}}]);
+//# sourceMappingURL=about.ed202c87.js.map
---
+++ frontend/src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>
+      초코콕 미니하이트쿠키
+    </div>
+  </div>
+</template>
