8196920ba59185735ca582e69f203d2cc6ad00dc
lyg0105 <lyg0105@naver.com>
2021-07-05 13:32:44 +0900

    gg

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,6 +6,7 @@
     <div>
       <input type="text" v-model="input1" />
       <button type="button" @click="getData" >Get</button>
+      <button type="button" @click="setData" >Set</button>
     </div>
   </div>
 </template>
@@ -28,7 +29,22 @@ export default {
   methods:{
     getData(){
       alert(this.input1);
+    },
+    setData(){
+      this.input1="12345";
     }
+  },
+  beforeCreate(){
+
+  },
+  created(){
+
+  },
+  beforeMount(){
+
+  },
+  mounted(){
+
   }
 }
 </script>
