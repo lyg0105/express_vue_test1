a14ee44b4f16862858285850a3daf1bc66f637ff
lyg0105 <lyg0105@naver.com>
2021-07-22 05:32:18 +0900

    vue study ing

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -2,7 +2,23 @@
   <div class="about">
     <h1>This is an about page</h1>
     <div>
-      초코콕 미니하이트쿠키
+      <span>여러 줄을 가지는 메시지:</span>
+      <p style="white-space: pre-line;">{{ message }}</p>
+      <br />
+      <textarea v-model="message" placeholder="여러줄을 입력해보세요"></textarea>
     </div>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data(){
+    return {
+      message:""
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/Data.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Data Page</h1>
+    {{people[0].name}}
+    <div v-for="persion in people" :key="persion.name">
+      <p>{{persion.name}}</p>
+      <p>{{persion.age}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Data',
+  data(){
+    return {
+      people:[
+        {name:"a",age:20},
+        {name:"b",age:22},
+        {name:"c",age:23},
+      ]
+    }
+  },
+  methods:{
+
+  },
+  watch:{
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
